0dd43bc3fcb3afd81372ed087841433cdbfa7dfc
KUNAL MAURYA <kunalmaurya178@gmail.com>
2025-07-06 00:50:16 +0530

    Fixes

--- src/my-trip/index.jsx
+++ src/my-trip/index.jsx
@@ -24,9 +24,9 @@ function MyTrips() {
     }
   return (
     <div>
-      <div className='sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10'>
+      <div className='sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10 mb-10'>
         <h2 className='font-bold text-3xl'>MyTrips</h2>
-        <div className='grid grid-cols-2 md:grid-cols-3 gap-5'>
+        <div className='grid grid-cols-2 md:grid-cols-3 gap-10'>
             {UserTrips?.length>0?UserTrips.map((trip,index)=>(
                 <UserTripCardItem trip={trip}/>
             )):[1,2,3,4,5,6].map((trip,index)=>{
